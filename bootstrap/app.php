3f5fa052f351062de21c369b3f158a3523a9afd5
Han4u <145313231+Han4u@users.noreply.github.com>
2026-05-28 20:44:09 +0700

    y

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
-        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
